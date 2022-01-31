a2813e7c4d0356f9f30ff5c5f7e28fafceef51da
laurajludwig <laura.jane.ludwig@gmail.com>
2022-01-31 14:35:47 -0800

    Add time of day questions

--- app.py
+++ app.py
@@ -70,15 +70,18 @@ industry_dict = {"Neither": "",
                  "Product": "4 4 8 4", 
                  "Retail": "2 6"}
 role_options = ['I consult with clients ONLY', 'I have an internal role ONLY', 'I consult AND have an internal role']
+time_types = ["Morning Lark", "Afternoon Eagle", "Night Owl"]
 
 class AutoStart(param.Parameterized):
     name_input = param.String(label= "My name is (first and last)")
     market_input = param.Selector(label="I live in", objects=list(live_dict.keys()))
+    time_type = param.Selector(label="I am a", objects=time_types)
     selected = param.Selector(objects=role_options)
 
     def panel(self):
         return pn.Column(pn.widgets.TextInput.from_param(self.param.name_input), pn.layout.VSpacer(), \
-                         "I live in", pn.Row(pn.widgets.RadioButtonGroup.from_param(self.param.market_input, button_type="success"), max_width=600, align='center'), pn.layout.Divider(), \
+                         "I live in", pn.Row(pn.widgets.RadioButtonGroup.from_param(self.param.market_input, button_type="success"), max_width=600, align='center'), pn.layout.VSpacer(), \
+                         "I am a", pn.Row(pn.widgets.RadioButtonGroup.from_param(self.param.time_type, button_type="success"), max_width=400, align='center'), pn.layout.Divider(), \
                          "Some Unifiers consult with their clients. Other Unifiers are internal and support our consultants (e.g., finance , operations, and people team). Then there are Unifiers who are hybrids and both consult as well as help shape the business (i.e., via leadership on practices, accounts, industries, or in an advocate role).",\
                          "Which one are you?", pn.Row(pn.widgets.RadioButtonGroup.from_param(self.param.selected, button_type="success"), max_width=600, align='center'))
 
@@ -97,6 +100,7 @@ class ConsultantQuestions(AutoStart):
     def output(self):
         consult_dict = {"name_input": self.name_input,
                        "live_input": self.market_input,
+                       "time_type": self.time_type,
                        "client_market": self.client_input,
                        "practice_input": self.practice_input,
                        "industry_input": self.industry_input, 
@@ -117,6 +121,7 @@ class InternalQuestions(AutoStart):
     def output(self):
         internal_dict = {"name_input": self.name_input,
                        "live_input": self.market_input,
+                       "time_type": self.time_type,
                        "client_market": "",
                        "practice_input": "",
                        "industry_input": "Neither", 
@@ -145,6 +150,7 @@ class HybridQuestions(AutoStart):
     def output(self):
         hybrid_dict = {"name_input": self.name_input,
                        "live_input": self.market_input,
+                       "time_type": self.time_type,
                        "client_market": self.client_input,
                        "practice_input": self.practice_input,
                        "industry_input": self.industry_input, 
@@ -160,6 +166,7 @@ class DrawSVG(AutoStart):
         #get and clean values from inputs 
         name = self.result['name_input']
         live_market = self.result['live_input']
+        time_type = self.result['time_type']
         client_market = self.result['client_market']
         practice = self.result['practice_input']
         industry = self.result['industry_input']
@@ -179,6 +186,20 @@ class DrawSVG(AutoStart):
         my_filename = name+'.svg'
         dwg = svgwrite.Drawing(my_filename, size=(400, 400), profile='full')
         dwg.add(dwg.rect(insert=(0,0), size=(400,400), fill=live_dict[live_market]))
+        
+        if time_type=="Afternoon Eagle":
+            pattern = dwg.defs.add(dwg.pattern(insert=(0,0), size=(100, 100), patternUnits="userSpaceOnUse"))
+            path = 'M11 18 c3.866 0 7 -3.134 7 -7 s-3.134 -7 -7 -7 -7 3.134 -7 7 3.134 7 7 7z m48 25 c3.866 0 7 -3.134 7 -7 s-3.134 -7 -7 -7 -7 3.134 -7 7 3.134 7 7 7z m-43 -7 c1.657 0 3 -1.343 3 -3 s-1.343 -3 -3 -3 -3 1.343 -3 3 1.343 3 3 3z m63 31 c1.657 0 3 -1.343 3 -3 s-1.343 -3 -3 -3 -3 1.343 -3 3 1.343 3 3 3z M34 90 c1.657 0 3 -1.343 3 -3 s-1.343 -3 -3 -3 -3 1.343 -3 3 1.343 3 3 3z m56 -76 c1.657 0 3 -1.343 3 -3 s-1.343 -3 -3 -3 -3 1.343 -3 3 1.343 3 3 3z M12 86 c2.21 0 4 -1.79 4 -4 s-1.79 -4 -4 -4 -4 1.79 -4 4 1.79 4 4 4z m28 -65 c2.21 0 4 -1.79 4 -4 s-1.79 -4 -4 -4 -4 1.79 -4 4 1.79 4 4 4z m23 -11 c2.76 0 5 -2.24 5 -5 s-2.24 -5 -5 -5 -5 2.24 -5 5 2.24 5 5 5z m-6 60 c2.21 0 4 -1.79 4 -4 s-1.79 -4 -4 -4 -4 1.79 -4 4 1.79 4 4 4z m29 22 c2.76 0 5 -2.24 5 -5 s-2.24 -5 -5 -5 -5 2.24 -5 5 2.24 5 5 5z M32 63 c2.76 0 5 -2.24 5 -5 s-2.24 -5 -5 -5 -5 2.24 -5 5 2.24 5 5 5z m57 -13 c2.76 0 5 -2.24 5 -5 s-2.24 -5 -5 -5 -5 2.24 -5 5 2.24 5 5 5z m-9 -21 c1.105 0 2 -.895 2 -2 s-.895 -2 -2 -2 -2 .895 -2 2 .895 2 2 2z M60 91 c1.105 0 2 -.895 2 -2 s-.895 -2 -2 -2 -2 .895 -2 2 .895 2 2 2z M35 41 c1.105 0 2 -.895 2 -2 s-.895 -2 -2 -2 -2 .895 -2 2 .895 2 2 2z M12 60 c1.105 0 2 -.895 2 -2 s-.895 -2 -2 -2 -2 .895 -2 2 .895 2 2 2z'
+            pattern.add(dwg.path(d=path, style="fill:#FFFFFF; fill-opacity=0.8"))
+        elif time_type=="Morning Lark":
+            pattern = dwg.defs.add(dwg.pattern(insert=(0,0), size=(52, 26), patternUnits="userSpaceOnUse"))
+            path = 'M10 10 c0 -2.21 -1.79 -4 -4 -4 c-3.314 0 -6 -2.686 -6 -6h2c0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4 v2 c-3.314 0 -6 -2.686 -6 -6 0 -2.21 -1.79 -4 -4 -4 -3.314 0 -6 -2.686 -6 -6z m25.464 -1.95 l8.486 8.486 -1.414 1.414 -8.486 -8.486 1.414 -1.414z'
+            pattern.add(dwg.path(d=path, style="fill:#FFFFFF; fill-opacity=0.8")) 
+        elif time_type=="Night Owl":
+            pattern = dwg.defs.add(dwg.pattern(insert=(0,0), size=(48, 64), patternUnits="userSpaceOnUse"))
+            path ='M48 28 v-4 L36 12 24 24 12 12 0 24 v4 l4 4 -4 4 v4 l12 12 12 -12 12 12 12 -12 v-4 l -4 -4 4 -4z M8 32 l-6 -6 10 -10 10 10 -6 6 6 6 -10 10 L2 38 l6 -6z m12 0 l4 -4 4 4 -4 4 -4 -4z m12 0 l-6 -6 10 -10 10 10 -6 6 6 6 -10 10 -10 -10 6 -6z M0 16 L10 6 4 0 h4 l4 4 4 -4 h4 l-6 6 10 10 L34 6 l-6 -6 h4 l4 4 4 -4 h4 l-6 6 10 10 v4 L36 8 24 20 12 8 0 20 v-4z m0 32 l10 10 -6 6 h4l4 -4 4 4 h4 l-6 -6 10 -10 10 10 -6 6 h4 l4 -4 4 4 h4 l-6 -6 10 -10 v-4 L36 56 24 44 12 56 0 44 v4z'
+        pattern.add(dwg.path(d=path, style="fill:#FFFFFF; fill-opacity=0.8"))
+        
         dwg.add(dwg.circle(center=(200,200), r=190, fill="#FFFFFF"))
 
         #consulting glyph
